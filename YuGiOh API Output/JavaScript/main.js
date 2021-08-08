5f866ab01ce0187a805c591c15f33887efed4089
Lionel Crafts <lionel.crafts@gmail.com>
2021-08-08 14:11:18 -0500

    Added way to ban/not ban cards with button clicks

--- YuGiOh API Output/JavaScript/main.js
+++ YuGiOh API Output/JavaScript/main.js
@@ -1,22 +1,139 @@
+/**
+ * Leave prompt
+ */
+ window.onbeforeunload = function() {
+    return "Please make sure you save before leaving!";
+  };
+  
+
 Main = {
+    _currentIndex: 0,
+    _currentCardInfo: {},
     _uniqueTypes: {}, // Helps with figuring out the C# part of things
     _uniqueBanListValues: {},
 
     generateString: function() {
-        //this._verifyCardsExist();
-        this._displayCardData();
+        if (Settings.verifyCardsExist) {
+            this._verifyCardsExist();
+        }
+        
+        if (Settings.cardSortingMode) {
+            this._startSortMode();
+        } else {
+            this._displayCardData();
+        }
     },
 
     /**
      * Only needs to be used if new cards are added
      */
     _verifyCardsExist: function() {
-        let allNames = DataFromAPI.map(x => x.name.toLowerCase());
+        let allNames = DataFromAPI.map(x => x.name);
         Object.keys(Data).forEach(function(cardName) {
-            if (!allNames.includes(cardName.toLowerCase())) {
+            if (!allNames.includes(cardName)) {
                 console.log(`${cardName} not found!`);
             }
         });
+    },
+
+    /**
+     * Starts the sort mode, where we will manually set cards as banned via tempban
+     */
+    _startSortMode: function() {
+        removeCssClass(document.getElementById("cardInformation"), "nodisp");
+        this._clearCardInfoDiv();
+
+        this._currentIndex = Settings.indexToStartAt;
+        this._displayCardInfoForNextCard();
+    },
+
+    /**
+     * Writes the current card to the div to copy later
+     * Does not mark the card as banned
+     */
+    onCardIsGoodClicked: function() {
+        let objectString = this._createObjectString(this._currentCardInfo.name, this._currentCardInfo);
+        document.getElementById("mainDiv").innerHTML += objectString;
+        this._displayCardInfoForNextCard();
+    },
+
+    /**
+     * Writes the current card to the div to copy later
+     * Marks the card as banned
+     */
+    onCardIsBadClicked: function() {
+        let objectString = this._createObjectString(this._currentCardInfo.name, this._currentCardInfo, true);
+        document.getElementById("mainDiv").innerHTML += objectString;
+        this._displayCardInfoForNextCard();
+    },
+
+    /**
+     * Displays the card information for the next card
+     */
+    _displayCardInfoForNextCard() {
+        this._currentCardInfo = this._getNextCard();
+        this._displayCardInfo(this._currentCardInfo)
+    },
+
+    /**
+     * Displays all the info from the card in the appropriate divs
+     */
+    _displayCardInfo(cardInfo) {
+        document.getElementById("indexValue").innerHTML = this._currentIndex;
+        document.getElementById("idValue").innerHTML = Data[cardInfo.name];
+        document.getElementById("nameValue").innerHTML = cardInfo.name;
+
+        document.getElementById("levelValue").innerHTML = cardInfo.level;
+        document.getElementById("raceValue").innerHTML = cardInfo.race;
+        document.getElementById("typeValue").innerHTML = cardInfo.type;
+        document.getElementById("attributeValue").innerHTML = cardInfo.attribute;
+
+        document.getElementById("atkValue").innerHTML = cardInfo.atk;
+        document.getElementById("defValue").innerHTML = cardInfo.def;
+        document.getElementById("archetypeValue").innerHTML = cardInfo.archetype;
+        document.getElementById("banlist_infoValue").innerHTML = cardInfo.banlist_info ? cardInfo.banlist_info.ban_tcg : "undefined";
+
+        document.getElementById("descriptionValue").innerHTML = cardInfo.desc;
+    },
+
+    /** Creates a clean slate for the card info div */
+    _clearCardInfoDiv: function() {
+        document.getElementById("indexValue").innerHTML = "<no value>";
+        document.getElementById("idValue").innerHTML = "<no value>";
+        document.getElementById("nameValue").innerHTML = "<no value>";
+
+        document.getElementById("levelValue").innerHTML = "<no value>";
+        document.getElementById("raceValue").innerHTML = "<no value>";
+        document.getElementById("typeValue").innerHTML = "<no value>";
+        document.getElementById("attributeValue").innerHTML = "<no value>";
+
+        document.getElementById("atkValue").innerHTML = "<no value>";
+        document.getElementById("defValue").innerHTML = "<no value>";
+        document.getElementById("archetypeValue").innerHTML = "<no value>";
+        document.getElementById("banlist_infoValue").innerHTML = "<no value>";
+
+        document.getElementById("descriptionValue").innerHTML = "<no value>";
+    },
+
+    /**
+     * Gets the next card to display - will skip over those not in the game
+     * @returns The card data to display
+     */
+    _getNextCard: function() {
+        let cardData = null;
+        do {
+            if (this._currentIndex > DataFromAPI.length - 1) {
+                break;
+            }
+
+            let currentCard = DataFromAPI[this._currentIndex];
+            if (Data[currentCard.name]) { // Only if the card is in the game should we use it
+                cardData = currentCard;
+            }
+            this._currentIndex++;
+        } while(cardData == null);
+
+        return cardData;
     },
 
     /**
@@ -26,7 +143,7 @@ Main = {
         let _this = this;
         let objectString = "";
         Object.keys(Data).forEach(function(cardName) {
-            let data = DataFromAPI.find(x => x.name.toLowerCase() === cardName.toLowerCase());
+            let data = DataFromAPI.find(x => x.name === cardName);
 
             if (!_this._uniqueTypes[data.type]) {
                 _this._uniqueTypes[data.type] = data.type;
@@ -36,22 +153,39 @@ Main = {
                 _this._uniqueBanListValues[data.banlist_info.ban_tcg] = data.banlist_info.ban_tcg;
             }
 
-            objectString += "{ ";
-            objectString += `"id": ${Data[cardName]}, `;
-            objectString += _this._getPropertyString(data, "name");
-            objectString += _this._getPropertyString(data, "type");
-            objectString +=  _this._getPropertyString(data, "attribute");
-            objectString += _this._getPropertyString(data, "level", true);
-            objectString += _this._getPropertyString(data, "race");
-            objectString +=  _this._getPropertyString(data, "atk", true);
-            objectString += _this._getPropertyString(data, "def", true);
-            objectString += _this._getPropertyString(data, "archetype");
-            objectString += _this._getPropertyString(data, "banlist_info");
-            objectString = objectString.substring(0, objectString.length - 2);
-            objectString += " },<br/>";
+            objectString += _this._createObjectString(cardName, data);
         });
 
-        document.body.innerHTML = objectString;
+        document.getElementById("mainDiv").innerHTML = objectString;
+    },
+
+    /**
+     * Creates the string for the card's JSON object
+     * @param {String} cardName - the name of the card
+     * @param {*} data - the card's data, from DataFromAPI
+     * @param {Boolean} banCard - whether to ban the card altogether
+     */
+    _createObjectString: function(cardName, data, banCard) {
+        let objectString = "{ ";
+        objectString += `"id": ${Data[cardName]}, `;
+        objectString += this._getPropertyString(data, "name");
+        objectString += this._getPropertyString(data, "type");
+        objectString +=  this._getPropertyString(data, "attribute");
+        objectString += this._getPropertyString(data, "level", true);
+        objectString += this._getPropertyString(data, "race");
+        objectString +=  this._getPropertyString(data, "atk", true);
+        objectString += this._getPropertyString(data, "def", true);
+        objectString += this._getPropertyString(data, "archetype");
+        objectString += this._getPropertyString(data, "banlist_info");
+
+        if (banCard) {
+            objectString += '"tempBan": true, ';
+        }
+        
+        objectString = objectString.substring(0, objectString.length - 2);
+        objectString += " },<br/>";
+
+        return objectString;
     },
 
     /**
---
+++ YuGiOh API Output/JavaScript/settings.js
@@ -0,0 +1,5 @@
+Settings = {
+    verifyCardsExist: false, // Only need to do this one time ever, unless new cards exist
+    cardSortingMode: true,
+    indexToStartAt: 0
+}
